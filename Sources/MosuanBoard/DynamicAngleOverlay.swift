7efbfcd715cee80924264300dc9e65f0cecb0bac
fanlaoshi027 <fanzeping@gmail.com>
2026-09-15 10:01:35 +0800

    chore: migrate dynamic angle shell

---
+++ Sources/MosuanBoard/DynamicAngleOverlay.swift
@@ -0,0 +1,6 @@
+import SwiftUI
+
+/// Retained as a compatibility shell while dynamic angles use the persistent Metal object path.
+struct DynamicAngleOverlay: View {
+    var body: some View { EmptyView() }
+}
